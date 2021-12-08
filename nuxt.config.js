bb8bca57648a24eb6af7939b83861cb3d3adaefb
Dejan Dimic <dejan.dimic@gmail.com>
2021-12-08 17:53:05 +0100

    fix(1): rendering headers

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,20 +1,12 @@
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
-  target: 'static',  
+  ssr: false,
+  target: 'static',
   docs: {
     primaryColor: '#E24F55'
   },
-
   router: { 
     base: '/githubpages-doc-guidelines/' 
-  },
-  build: {
-    publicPath: '/_nuxt/',
-    babel:{
-      plugins: [
-        ['@babel/plugin-proposal-private-methods', { loose: true }]
-      ]
-    }
-  },
+  }
 })
